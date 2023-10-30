5928b4cfd4fd1a91d3b5c01eed2095855e86161e
AshotNik <ashot.nik777@gmail.com>
2023-10-30 23:43:22 +0400

    Comented: pylint 7.22/10

--- Speech_Recognition.py
+++ Speech_Recognition.py
@@ -1,51 +1,48 @@
 #!/usr/bin/python3
+
 # Imports
 import subprocess
-
+import os
 try:
     import speech_recognition as sr
 except ImportError:
-    print("speech_recognition library not found. Installing...")
+    print("Speech Recognition library not found. Installing...")
     subprocess.check_call(["pip", "install", "speech_recognition"])
     import speech_recognition as sr
 
-try:
-    import os
-except ImportError:
-    print("os library not found. Installing...")
-    subprocess.check_call(["pip", "install", "os"])
-    import os
-
+# Constants
+OPEN_COMMAND = "open"
+CLOSE_COMMAND = "close"
 
 # Open/Close
 def command(voic):
-    com=voic.split()
+    com = voic.split()
     try:
-        if com[0]=="open"   :
+        if com[0] == OPEN_COMMAND:
             subprocess.Popen([com[1].lower()])
-        elif com[0]=='close':
+        elif com[0] == CLOSE_COMMAND:
             os.system(f"killall -9 {com[1].lower()}")
     except sr.UnknownValueError:
         print("Google Web Speech API could not understand audio.")
-    except sr.RequestError as e:
-        print(f"Could not request results from Google Web Speech API; {e}")
+    except sr.RequestError as error:
+        print(f"Could not request results from Google Web Speech API: {error}")
     recognation()
 
-# Recognation/Speak commands
-def recognation():
+# Recognition/Speak commands
+def recognition():
     with sr.Microphone() as mic:
-            recognize=sr.Recognizer()
-            recognize.adjust_for_ambient_noise(mic)
-            print("Please tell  open or close(open Firefox) or break") 
-            audio=recognize.listen(mic)
-    
-    text = recognize.recognize_google(audio)
-    print(text)
-    if not text.lower()=="break":
+        recognizer = sr.Recognizer()
+        recognizer.adjust_for_ambient_noise(mic)
+        print("Please say 'open' or 'close' (e.g., 'open Firefox') or 'break' to exit.")
+        audio = recognizer.listen(mic)
+
+    text = recognizer.recognize_google(audio)
+    print(f"Recognized command: {text}")
+    if not text.lower() == "break":
         command(text)
-        
 
 def main():
-    recognation() 
-    
-main()
+    recognition()
+
+if __name__ == "__main__":
+    main()
