3f3f290290ce80ab35f8fe25a63049e39756f94e
AshotNik <ashot.nik777@gmail.com>
2023-10-30 23:21:42 +0400

    comented and pylint 7.92/10

--- send_mail.py
+++ send_mail.py
@@ -4,11 +4,11 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
 # Email configuration
-sender_email = "sendmail071@gmail.com"
-receiver_email = "sendmail071@gmail.com"
-sender_password = "pvyh cjsc eskb wgar"
-subject = input("Enter subject of the Email: ")
-message = input("Enter the text for Email: ")
+SENDER_EMAIL = "sendmail071@gmail.com"
+RECEIVER_EMAIL = "tigran.barsegyan@gmail.com"
+SENDER_PASSWORD = "pvyh cjsc eskb wgar"  # This should be a valid password for the sender's email account
+SUBJECT = input("Enter subject of the Email: ")  # Prompt user for email subject
+MESSAGE = input("Enter the text for Email: ")  # Prompt user for email message
 
 # Create a function to send an email
 def send_email(sender_email, receiver_email, password, subject, message):
@@ -17,22 +17,22 @@ def send_email(sender_email, receiver_email, password, subject, message):
     msg['From'] = sender_email
     msg['To'] = receiver_email
     msg['Subject'] = subject
-    print(msg)
-    msg.attach(MIMEText(message, 'plain'))
-    print(msg)
 
-    # Connect to the SMTP servver and send
+    msg.attach(MIMEText(message, 'plain'))
+
+    # Connect to the SMTP server and send
     try:
-        server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-        server.login(sender_email, password)
+        server = smtplib.SMTP_SSL('smtp.gmail.com', 465)  # Connect to Gmail's SMTP server using SSL on port 465
+        server.login(sender_email, password)  # Log in to the sender's email account
 
         # Send the email
-        server.sendmail(sender_email, receiver_email, msg.as_string())
+        server.sendmail(sender_email, receiver_email, msg.as_string())  # Send the email
         print("Email sent successfully!")
 
     except smtplib.SMTPException as e:
         print(f"SMTP Exception: {str(e)}")
     finally:
-        server.quit()
+        server.quit()  # Quit the server
 
-send_email(sender_email, receiver_email, sender_password, subject, message)
+# Call the send_email function to send the email
+send_email(SENDER_EMAIL, RECEIVER_EMAIL, SENDER_PASSWORD, SUBJECT, MESSAGE)
